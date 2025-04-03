7b9baeb9ddd75284b6d66b78cc2db75b34e44eeb
Tsptds <152429392+Tsptds@users.noreply.github.com>
2025-04-03 23:57:01 +0300

    upload fo4 variant

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using Fallout4Launcher;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fallout_4_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 frmstngs = new Form2();
+
+        void StopAutorun()
+        {
+            timer1.Stop();
+            lblTimer.Visible=false;
+            lblTimer2.Visible=false;
+            if (Settings1.Default.AutorunS) lblStop.Visible = true;
+        }
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Beige;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.DarkGray;
+        }
+
+        private void label2_MouseEnter(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.Beige;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.DarkGray;
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.Beige;
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.DarkGray;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled) StopAutorun();
+            if (!File.Exists(Settings1.Default.btn1S))
+            {
+                MessageBox.Show("Can't find \""+ Settings1.Default.btn1S+ "\"\nSelect a File From the Settings", "Error");
+            }
+            else
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = Settings1.Default.btn1S;
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+                Environment.Exit(0);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) StopAutorun();
+            if (!File.Exists(Settings1.Default.btn2S))
+            {
+                MessageBox.Show("Can't find \"" + Settings1.Default.btn2S + "\"\nSelect a File From the Settings", "Error");
+            }
+            else
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = Settings1.Default.btn2S;
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTimer2.Text = Convert.ToString(Convert.ToInt16(lblTimer2.Text)-1);
+
+            if(lblTimer2.Text=="0")
+            {
+                label1_Click(sender, e);
+            }
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.Beige;
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.DarkGray;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) StopAutorun();
+            frmstngs.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = Settings1.Default.label1S;
+            label2.Text = Settings1.Default.label2S;
+
+            if(Settings1.Default.AutorunS)
+            {
+                timer1.Enabled = true;
+                lblTimer.Visible = true;
+                lblTimer2.Visible = true;
+                lblTimer2.Text = Settings1.Default.Counter.ToString();
+            }
+        }
+
+
+        // MOVE BORDERLESS FORM
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fallout4Launcher
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        static string Search()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Filter = "All files (*.*)|*.*|exe files (*.exe)|*.exe|Shortcuts (*.lnk)|*.lnk";
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else return string.Empty;
+        }
+
+        static void DEFAULTS()
+        {
+            Settings1.Default.AutorunS = true;
+            Settings1.Default.label1S="PLAY";
+            Settings1.Default.label2S = "Open Launcher";
+            Settings1.Default.btn1S = "Game File";
+            Settings1.Default.btn2S = "Launcher File";
+            Settings1.Default.Counter = 15;
+            Settings1.Default.Save();
+        }
+        void Current()
+        {
+            textBox1.Text = Settings1.Default.label1S;
+            textBox2.Text = Settings1.Default.label2S;
+            richTextBox1.Text = Settings1.Default.btn1S;
+            richTextBox2.Text = Settings1.Default.btn2S;
+            chkAutorun.Checked = Settings1.Default.AutorunS;
+            txtCount.Text = Settings1.Default.Counter.ToString();
+            txtCount.ReadOnly = !Settings1.Default.AutorunS;
+        }
+
+        string path1=Settings1.Default.btn1S;
+        string path2=Settings1.Default.btn2S;
+        bool autorun = Settings1.Default.AutorunS;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Current();
+        }
+
+
+
+        private void chkAutorun_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAutorun.Checked)
+            {
+                autorun = true;
+                txtCount.ReadOnly = false;
+            }
+            else
+            {
+                autorun = false;
+                txtCount.ReadOnly = true;
+            }
+        }
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Current();
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void btndefault_Click(object sender, EventArgs e)
+        {
+            DEFAULTS();
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+            Environment.Exit(0);
+        }
+
+        private void btnSelectfile1_Click(object sender, EventArgs e)
+        {
+            string path= Search();
+            if (path == "") MessageBox.Show("No Path Selected","Cancelled");
+            else path1 = path;
+            richTextBox1.Text = path;
+        }
+
+        private void btnSelectFile2_Click(object sender, EventArgs e)
+        {
+            string path = Search();
+            if (path == "") MessageBox.Show("No Path Selected","Cancelled");
+            else path2 = path;
+            richTextBox2.Text = path;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            int cdT = Convert.ToInt16(txtCount.Text);
+            if (cdT < 3 || cdT > 30)
+            {
+                MessageBox.Show("Interval for countdown is 3-30 seconds", "Warning");
+                return;
+            }
+            else
+            {
+                Settings1.Default.label1S = textBox1.Text;
+                Settings1.Default.label2S = textBox2.Text;
+                Settings1.Default.btn1S = path1;
+                Settings1.Default.btn2S = path2;
+                Settings1.Default.AutorunS = autorun;
+                Settings1.Default.Counter = Convert.ToInt16(txtCount.Text);
+                Settings1.Default.Save();
+                System.Diagnostics.Process.Start(Application.ExecutablePath);
+                Environment.Exit(0);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.MaxLength = 13;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.MaxLength = 13;
+        }
+
+        private void txtCount_TextChanged(object sender, EventArgs e)
+        {
+            txtCount.MaxLength = 2;
+        }
+    }
+}
